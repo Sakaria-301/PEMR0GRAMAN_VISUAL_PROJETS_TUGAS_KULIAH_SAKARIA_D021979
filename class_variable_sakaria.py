05bc98688ac466686d5db9e0d59dfd0e0f45f208
Sakaria-301 <101163563+Sakaria-301@users.noreply.github.com>
2022-04-20 13:00:34 +0700

    Add files via upload

---
+++ class_variable_sakaria.py
@@ -0,0 +1,24 @@
+#sakaria_D0219379
+
+class Manusia:
+    nama : None
+    nim : None
+
+manusiaRakus = Manusia()
+manusiaRakus.nama = "jean"
+manusiaRakus.nim = "D0219028"
+
+manusiaManis = Manusia()
+manusiaManis.nama = "david"
+manusiaManis.nim = "D0219008"
+
+manusiaCuek = Manusia()
+manusiaCuek .nama = "sahida"
+manusiaCuek .nim = "D0290888"
+
+print("manusiaRakus : \n"+manusiaRakus.nama, "\n"+manusiaRakus.nim)
+print("==========================")
+print("Manusia Manis: \n"+manusiaManis.nama, "\n"+manusiaManis.nim)
+print("==========================")
+print("manusiaCuek  : \n"+manusiaCuek.nama, "\n"+manusiaCuek.nim)
+print("\n \n \n TERIMAKASIH")
